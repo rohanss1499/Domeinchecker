44fafdf97ae2516981967dd5fe4e328b0e25ee1f
rohanss1499 <37865368+rohanss1499@users.noreply.github.com>
2018-03-29 16:50:02 +0200

    Final Version

--- domeincheck.php
+++ domeincheck.php
@@ -1,5 +1,31 @@
 	<?php 
 
-	  $tldList = array('nl', 'org', 'net', 'co.uk', 'com');
+	  $tldList = array(
+	      'nl',
+          'org',
+          'net',
+          'co.uk',
+          'com',
+          'eu',
+          'be',
+          'info',
+          'de',
+          'online',
+          'nu',
+          'fr',
+          'it',
+          'tech',
+          'store',
+          'io',
+          'amsterdam',
+          'ch',
+          'at',
+          'fri',
+          'shop',
+          'es',
+          'uk',
+          'co',
+          'cloud'
+      );
 
 	?>
