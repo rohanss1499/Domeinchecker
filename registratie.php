e057c138b89bae5db8423968434e50b77fe40788
rohanss1499 <37865368+rohanss1499@users.noreply.github.com>
2018-04-13 10:31:33 +0200

    Final Version

--- registratie.php
+++ registratie.php
@@ -18,9 +18,9 @@
     <!--Favicon-->
     <link href="https://compleet.it/favicon.ico" rel="icon" type="image/x-icon"/>
     <!--CSS-->
+    <link rel="stylesheet" href="css/stylesheet.css" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/stylesheet.css" type="text/css">
 </head>
 <body>
 
